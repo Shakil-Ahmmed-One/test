e6d32ab81963a5171c0f7d81514d82827f95eb0a
Shakil Ahmmed <shakilahmmedwakil.saw+one@gmail.com>
2026-01-22 16:33:29 +0600

    Add Prisma extension and update database seeding logic

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,12 +1,4 @@
-import { PrismaClient } from '../src/generated/prisma/client.js'
-
-import { PrismaPg } from '@prisma/adapter-pg'
-
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL!,
-})
-
-const prisma = new PrismaClient({ adapter })
+import { prisma } from '@/db.js'
 
 async function main() {
   console.log('🌱 Seeding database...')
--- src/db.ts
+++ src/db.ts
@@ -1,17 +1,6 @@
-import { PrismaClient } from './generated/prisma/client.js'
+import { PrismaClient } from "./generated/prisma/client.js";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
-import { PrismaPg } from '@prisma/adapter-pg'
-
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL!,
-})
-
-declare global {
-  var __prisma: PrismaClient | undefined
-}
-
-export const prisma = globalThis.__prisma || new PrismaClient({ adapter })
-
-if (process.env.NODE_ENV !== 'production') {
-  globalThis.__prisma = prisma
-}
+export const prisma = new PrismaClient({
+  accelerateUrl: process.env.DATABASE_URL!,
+}).$extends(withAccelerate());
